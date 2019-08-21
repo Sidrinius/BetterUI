95a1f868735df585a82e2124d32bc59e3e7c8212
goobsnake <39643499+goobsnake@users.noreply.github.com>
2019-08-21 08:27:11 -0700

    Update Module.lua
    I incorrectly moved the init settings

--- Modules/CIM/Module.lua
+++ Modules/CIM/Module.lua
@@ -5,7 +5,9 @@ function BETTERUI.CIM.InitModule(m_options)
 	m_options["attributeIcons"] = true
 	m_options["triggerSpeed"] = 10
 	m_options["enhanceCompat"] = false
+	m_options["skinSize"] = "Default"
 	m_options["rhScrollSpeed"] = 50
+	m_options["tooltipSize"] = "Default"
 	return m_options
 end
 
