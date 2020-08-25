f9ec8285618866a8dd7d2b03747175b811a7e313
goobsnake <goobsnake@gmail.com>
2020-08-25 12:05:25 -0700

    Version update

--- Globals.lua
+++ Globals.lua
@@ -3,7 +3,7 @@ BETTERUI = {
 }
 
 BETTERUI.name = "BetterUI"
-BETTERUI.version = "2.73"
+BETTERUI.version = "2.74"
 
 -- Program Global (scope of BETTERUI, though) variable initialization
 BETTERUI.WindowManager = GetWindowManager()
